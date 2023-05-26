d15df11600a012522593e0de0d221ebd58360b79
CourtneyAlv <voiceoftruth42@gmail.com>
2023-05-25 20:22:59 -0700

    "6"

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - base 10 again
+ * Return: 0
+*/
+	int main(void)
+{
+	int x = 48;
+
+	while
+	(x < 58)
+{
+	putchar(x);
+	(x++);
+}
+	putchar('\n');
+	return (0);
+}
