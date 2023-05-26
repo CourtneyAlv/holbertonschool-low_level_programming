a0ce9dae854abd00d340957c4f4b3a5b64bdae6b
CourtneyAlv <voiceoftruth42@gmail.com>
2023-05-25 17:52:22 -0700

    "2"

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints
+ * Return: 0
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while
+
+		(ch <= 'z')
+	{
+		putchar(ch);
+
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+
+}
