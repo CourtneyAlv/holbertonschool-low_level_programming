1d6978924ea3d33aa5512e9b78c536f7b022ecf8
CourtneyAlv <voiceoftruth42@gmail.com>
2023-05-25 21:23:34 -0700

    "8"

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - hexadecimal
+ * Return: 0
+*/
+int main(void)
+{
+	int h;
+
+	for
+	(h = 48; h <= 58; h++)
+{
+	putchar(h);
+}
+	for
+	(h = 'a'; h <= 'f'; h++)
+{
+	putchar(h);
+}
+	putchar('\n');
+	return (0);
+}
