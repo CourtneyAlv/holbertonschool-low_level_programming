dfb84cf0effd44a5f2be1ed62279b3d58e633e4f
CourtneyAlv <voiceoftruth42@gmail.com>
2023-05-31 10:04:06 -0700

    "0"

---
+++ more_function_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _supper - Write a function that checks for uppercase character
+ * @c: find c
+ * Return: 0
+*/
+int _isupper(int c)
+{
+	if
+
+	(c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+}
