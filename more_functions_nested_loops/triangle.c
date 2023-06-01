246776b062ca2f26382414bcbfb1115ede58566a
CourtneyAlv <voiceoftruth42@gmail.com>
2023-06-01 12:57:52 -0700

    "8"

---
+++ more_functions_nested_loops/triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - Write a function that prints a square
+ * @size: size of the square
+ * Return: 0
+*/
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if
+	(size <= 0)
+	_putchar('\n');
+
+	for (i = 0; i < size; i++)
+{
+	for (j = 0; j < i; j++)
+	_putchar('#');
+	_putchar('\n');
+}
+}
