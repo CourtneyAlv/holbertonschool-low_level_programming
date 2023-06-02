1298a11f0f2e9316bb8fbebecfa70e364a2e3b97
CourtneyAlv <voiceoftruth42@gmail.com>
2023-06-01 19:23:03 -0700

    "9"

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 {
 	if
-	(i % 3 == 0)
-	printf("Fizz");
-
+	(i % 3 == 0 && i % 5 == 0)
+	printf("FizzBuzz");
 
 	else if
 	(i % 5 == 0)
 	printf("Buzz");
 
 	else if
-	(i % 3 == 0 && i % 5 == 0)
-	printf("FizzBuzz");
+	(i % 3 == 0)
+	printf("Fizz");
 
 	else
 	printf("%d", i);
