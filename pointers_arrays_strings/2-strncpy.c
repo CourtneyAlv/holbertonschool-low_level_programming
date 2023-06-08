dba57176a4dcfd708417c78f7f396a5434567a8a
CourtneyAlv <voiceoftruth42@gmail.com>
2023-06-08 13:31:16 -0700

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - Write a function that copies a string.
+ * @dest: find value
+ * @src: find value
+ * @n: max number of bytes
+ * Return: 0
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while
+	(a < n && src[a] != 0)
+{
+	a++;
+}
+	while
+	(a < n)
+{
+	dest[a] = '\0';
+	a++;
+}
+	return (dest);
+}
