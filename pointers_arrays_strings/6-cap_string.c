cbc730deb442427070beb4ef4a3eccd95d421d4a
CourtneyAlv <voiceoftruth42@gmail.com>
2023-06-09 17:30:48 -0700

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,18 @@
 #include "main.h"
+#include <ctype.h>
 /**
- *
+ * cap_string - Write a function that capitalizes all words of a string.
+ * @str: capitalizing first letter of word.
+ * Return: str
+*/
+char *cap_string(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c++)
+{
+	if (c == 0 || isspace(str[c - 1]))
+		str[c] = toupper(str[c]);
+}
+	return (str);
+}
