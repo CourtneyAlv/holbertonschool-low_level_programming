85de75619d925a64728c5cb181db31b56125322c
CourtneyAlv <voiceoftruth42@gmail.com>
2023-06-22 18:23:43 -0700

    using strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,1 +1,27 @@
-#include
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strdup - function that returns a pointer to a newly allocated space in mem
+ * @str: functions input parameter
+ * Return: pointer to new string
+*/
+char *_strdup(char *str)
+{
+	char *d;
+	size_t l;
+
+	if (str == NULL)
+	return (NULL);
+
+	l = strlen(str) + 1;
+	d = malloc(l);
+
+	if (d == NULL)
+	return (NULL);
+
+	memcpy(d, str, l);
+
+	return (d);
+}
