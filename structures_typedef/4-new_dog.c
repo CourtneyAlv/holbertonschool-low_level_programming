735b1960efb22b22f99e87e029e9a70d3428a02a
CourtneyAlv <voiceoftruth42@gmail.com>
2023-06-29 22:35:49 -0700

    Write a function that frees dogs.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -18,7 +18,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	return (NULL);
 
 	new_dog->name = strdup(name);
-	if (new_dog->name == (NULL))
+	if (new_dog->name == NULL)
 	{
 
 		free(new_dog);
