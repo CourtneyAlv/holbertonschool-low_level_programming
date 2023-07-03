c4db65355a507f80fd653aee5a050d7281e0ad96
CourtneyAlv <voiceoftruth42@gmail.com>
2023-07-02 21:03:17 -0700

    Write a program that performs simple operations

---
+++ function_pointers/3-main.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -
+ * @argc:
+ * @argv:
+ * Return:
+*/
+int main(int__attribute__((__unused__))argc, char *argv[])
+{
+	int i, j;
+	char *a;
+
+	if (argc != 4)
+{
+	printf("Error\n"
