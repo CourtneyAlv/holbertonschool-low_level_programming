7bbe10e10d4145f6b5f5925c13a5d219897a4679
CourtneyAlv <voiceoftruth42@gmail.com>
2023-07-14 20:59:32 -0700

    Write a function that adds a new node at the beginning of a list_t list.

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,27 +10,29 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	if (str == NULL)
-		/* Return NULL if input strng is NULL */
-		return (NULL);
+	/* Return NULL if input strng is NULL */
+	return (NULL);
 	/* Allocate memory for the new node */
 	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
-		/* Return NULL if input string is NULL */
-		return (NULL);
+	/* Return NULL if input string is NULL */
+	return (NULL);
 	/* Duplicate the input string */
 	new_node->str = strdup(str);
-	if (new_node-> == NULL)
-		{
-			free(new_node);
-			/* Return NULL if string duplication fails */
-			return(NULL);
-		}
-	/* Calculate the length of the string */
-	new_node->len = strlen(str);
-	/* Set the next pointer of the new node to the current head */
-	new_node->next = *head;
-	/* Update the head pointer to point to the new node */
-	*head = new_node;
-	/* Return the address of the new node */
-	return (new_node);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		/* Return NULL if string duplication fails */
+		return (NULL);
+	}
+		/* Calculate the length of the string */
+		new_node->len = strlen(str);
+		/* Set the next pointer of the new node to the current head */
+		new_node->next = *head;
+		/* Update the head pointer to point to the new node */
+		*head = new_node;
+		/* Return the address of the new node */
+		return (new_node);
 }
