23b06ca128e7af02b7bee35ed6767f53db05005d
CourtneyAlv <voiceoftruth42@gmail.com>
2023-07-23 00:54:19 -0700

    Write a function that inserts a new node at a given position.

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,8 +1,21 @@
 #include "lists.h"
 /**
- *sum_dlistint-
- *@head:
- *Return:
+ *sum_dlistint-returns the sum of all the data (n) of a dlistint_t linked list
+ *@head: a pointer to the head
+ *Return: return 0 if the list is empty
 */
 int sum_dlistint(dlistint_t *head)
 {
+	/* initialize a variable to store the sum */
+	int sum = 0;
+
+	while (head != NULL)
+{
+	/* add the current nodes value to the sum */
+	sum += head->n;
+	/* move to the next node */
+	head = head->next;
+}
+	/* returns the sum */
+	return (sum);
+}
