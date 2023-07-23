4723ad1a0a8d445bac4d99cda1ed466dfec21edc
CourtneyAlv <voiceoftruth42@gmail.com>
2023-07-22 17:02:03 -0700

    Added all files

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,8 @@
+#include "hash_tables.h"
+/**
+ *hash_djb2-
+ *@str:
+ *Return:
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,8 @@
+#include "hash_tables.h"
+/**
+ *key_index-
+ *@key:
+ *@size:
+ *Return:
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
