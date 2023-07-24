6e1c2f727ede25ed5772026b9540a6ded6225f21
CourtneyAlv <voiceoftruth42@gmail.com>
2023-07-23 22:52:57 -0700

    fixed betty

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -6,10 +6,10 @@
  *Return: the index at which the key should be stored in the hash table array
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{	
+{
 	/* calculates the hash value using the hash_djb2 function */
 	unsigned long int hash_value = hash_djb2(key);
-	
+
 	/* use the hash value to determine the index in the hash table array */
 
 	unsigned long int index = hash_value % size;
