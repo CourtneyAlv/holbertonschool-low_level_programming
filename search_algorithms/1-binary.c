de8995e7e0cd556c170b7804781a645bde74b299
CourtneyAlv <voiceoftruth42@gmail.com>
2023-08-18 13:50:54 -0700

    working on 1-binary.c

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,10 +1,49 @@
 #include "search_algos.h"
 /**
- *
- *
- *
- *
+ * binary_search- finding a value in an array using binary search
+ * @array: is a pointer to the first element
+ * @size: is the number of elements in tharrya
+ * @value: the value to be searched for
+ * Return: index where value is located, -1 if not found
 */
 int binary_search(int *array, size_t size, int value)
 {
+	int left = 0; /* index of left boundary */
+	int right = size - 1; /* index of right boundary */
+	int mid = left + (right - left) / 2; /* calculate middle index */
+	int i;
 
+	if (array == NULL)
+	{
+	return (-1);
+	}
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+
+			if (i < right)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+
+	if (array[mid] == value)
+	{
+		return (mid); /* return index if value is found */
+	}
+	else if (array[mid] < value)
+	{
+		left = mid + 1; /* adjust the left boundary */
+	}
+	else
+	{
+		right = mid - 1; /* adjust the right boundary */
+	}
+	return (-1);
+}
